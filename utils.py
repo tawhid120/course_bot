642ddb3bca8f406e40a7e3867d03f87c5fb049af
MD TAWHID ISLAM <lorddanju@gmail.com>
2026-04-11 09:49:25 +0600

    Add logging configuration in utils.py

---
+++ utils.py
@@ -0,0 +1,12 @@
+# utils.py
+# Copyright @YourChannel
+
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+LOGGER = logging.getLogger("CourseBot")
